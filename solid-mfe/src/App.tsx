367586099b0d1924642b1d39cb570e7552a0025a
scrawnyColeman <thomashutchinson450@gmail.com>
2022-09-23 01:57:24 +0100

    wip solid-js

---
+++ solid-mfe/src/App.tsx
@@ -0,0 +1,4 @@
+import { render } from "solid-js/web";
+
+const App = () => <button></button>;
+render(App, document.getElementById("app"));
---
+++ solid-mfe/webpack.config.js
@@ -0,0 +1,67 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const deps = require("./package.json").dependencies;
+module.exports = {
+  output: {
+    publicPath: "http://localhost:8084/",
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 8084,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+
+        loader: "babel-loader",
+        options: {
+          presets: [
+            "@babel/preset-typescript",
+            "babel-preset-solid",
+            "@babel/preset-env",
+          ],
+          plugins: [["@babel/transform-runtime"]],
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "solid_mfe",
+      filename: "remoteEntry.js",
+      remotes: {},
+      exposes: {},
+      shared: {
+        ...deps,
+        "solid-js": {
+          singleton: true,
+          requiredVersion: deps["solid-js"],
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
